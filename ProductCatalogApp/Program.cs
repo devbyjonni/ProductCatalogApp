18a53c25c5e2c5262e53f6f67c521625b98a9240
Jonni Åkesson <devbyjonni@gmail.com>
2025-03-20 19:55:41 +0100

    Initial commit with .NET .gitignore

---
+++ ProductCatalogApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProductCatalogApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Dotnet Console App is Running! 🚀");
+        }
+    }
+}
+
